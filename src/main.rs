c1cf55de0141dd42a9a174cd3c2148493a6e1d8a
Carlo Supina <cdsupina@gmail.com>
2023-02-04 00:27:16 -0600

    add window with resolution and physics plugin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+
+    app.add_plugins(
+        DefaultPlugins
+            .set(WindowPlugin {
+                window: WindowDescriptor {
+                    width: 854.0,
+                    height: 480.0,
+                    ..Default::default()
+                },
+                ..Default::default()
+            })
+            .set(ImagePlugin::default_nearest()),
+    )
+    .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(10.0));
+
+    app.run();
 }
