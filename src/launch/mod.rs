06fb914de48e9489bfa614d294721337a37f41f0
Carlo Supina <cdsupina@gmail.com>
2023-02-04 18:24:35 -0600

    add game states, music, asset loading

--- src/launch/mod.rs
+++ src/launch/mod.rs
@@ -1,8 +1,6 @@
-use crate::louse;
+use crate::{assets, louse};
 use bevy::prelude::*;
 use bevy_kira_audio::prelude::*;
-use bevy_rapier2d::prelude::*;
-use rand::{thread_rng, Rng};
 
 #[derive(Resource, Default, Debug)]
 pub struct LaunchResource {
@@ -11,16 +9,13 @@ pub struct LaunchResource {
 }
 
 pub fn fling_louse_system(
-    asset_server: Res<AssetServer>,
     windows: Res<Windows>,
     camera: Query<(&Camera, &GlobalTransform), With<crate::MainCamera>>,
     mouse_button: Res<Input<MouseButton>>,
-    mut commands: Commands,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-    time: Res<Time>,
     mut spawn_louse_events: EventWriter<louse::SpawnLouseEvent>,
     mut fling_resource: ResMut<LaunchResource>,
     audio_channel: Res<AudioChannel<crate::SoundEffectsAudioChannel>>,
+    game_assets: Res<assets::GameAssets>,
 ) {
     if mouse_button.just_pressed(MouseButton::Left) {
         let curr_window = windows.get_primary().unwrap();
@@ -31,7 +26,7 @@ pub fn fling_louse_system(
 
         if initial_position.x < crate::FIRE_LINE {
             fling_resource.initial_position = Some(initial_position);
-            audio_channel.play(asset_server.load("sounds/slingshot_pull.mp3"));
+            audio_channel.play(game_assets.slingshot_pull_sound.clone());
         }
     } else if mouse_button.just_released(MouseButton::Left) {
         if let Some(initial_position) = fling_resource.initial_position {
@@ -41,13 +36,10 @@ pub fn fling_louse_system(
             let final_position =
                 get_cursor_physics_position(camera, camera_gl_transform, curr_window);
 
-            println!("Initial Postion: {}", initial_position);
-            println!("Final Position: {}", final_position);
-
             let velocity = (initial_position - final_position) * fling_resource.velocity_multiplier;
             audio_channel.stop();
 
-            audio_channel.play(asset_server.load("sounds/slingshot_release.mp3"));
+            audio_channel.play(game_assets.slingshot_release_sound.clone());
             spawn_louse_events.send(louse::SpawnLouseEvent {
                 position: final_position,
                 velocity,
---
+++ src/states/mod.rs
@@ -0,0 +1,228 @@
+use std::time::Duration;
+
+use bevy::{app::AppExit, prelude::*};
+use bevy_kira_audio::prelude::*;
+use bevy_rapier2d::prelude::*;
+
+use crate::assets;
+
+// states of the game
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum AppStates {
+    LoadingMainMenu,
+    MainMenu,
+    PauseMenu,
+    LoadingGame,
+    Game,
+    GameOver,
+    Victory,
+}
+
+// used for tagging entities that are part of the game state
+#[derive(Component)]
+pub struct AppStateComponent(pub AppStates);
+
+// remove entities tagged for the current app state
+pub fn clear_state_system(
+    mut commands: Commands,
+    mut despawn_entities_query: Query<(Entity, &AppStateComponent)>,
+    app_state: Res<State<AppStates>>,
+) {
+    for (entity, entity_app_state) in despawn_entities_query.iter_mut() {
+        if *app_state.current() == entity_app_state.0 {
+            commands.entity(entity).despawn_recursive();
+        }
+    }
+}
+
+// Start the game by entering the game loading state
+pub fn start_game_system(
+    mut keyboard_input: ResMut<Input<KeyCode>>,
+    mut app_state: ResMut<State<AppStates>>,
+) {
+    // check for keyboard or gamepad input
+    let mut start_input = keyboard_input.just_released(KeyCode::Return)
+        || keyboard_input.just_released(KeyCode::Space);
+
+    // if input read enter the game loading state
+    if start_input {
+        // set the state to loading game state
+        app_state.set(AppStates::LoadingGame).unwrap();
+
+        // play sound effect
+        //audio_channel.play(asset_server.load("sounds/menu_input_success.wav"));
+
+        // reset input
+        keyboard_input.reset(KeyCode::Return);
+        keyboard_input.reset(KeyCode::Space);
+    }
+}
+
+pub fn start_gameover_system(
+    mut keyboard_input: ResMut<Input<KeyCode>>,
+    mut app_state: ResMut<State<AppStates>>,
+) {
+    // check for keyboard or gamepad input
+    let input = keyboard_input.just_released(KeyCode::L);
+
+    // if input read enter the game loading state
+    if input {
+        // set the state to loading game state
+        app_state.set(AppStates::GameOver).unwrap();
+
+        // play sound effect
+        //audio_channel.play(asset_server.load("sounds/menu_input_success.wav"));
+
+        // reset input
+        keyboard_input.reset(KeyCode::L);
+    }
+}
+
+pub fn start_victory_system(
+    mut keyboard_input: ResMut<Input<KeyCode>>,
+    mut app_state: ResMut<State<AppStates>>,
+) {
+    // check for keyboard or gamepad input
+    let input = keyboard_input.just_released(KeyCode::W);
+
+    // if input read enter the game loading state
+    if input {
+        // set the state to loading game state
+        app_state.set(AppStates::Victory).unwrap();
+
+        // play sound effect
+        //audio_channel.play(asset_server.load("sounds/menu_input_success.wav"));
+
+        // reset input
+        keyboard_input.reset(KeyCode::W);
+    }
+}
+
+/// Quit the game if quit input read
+pub fn quit_game_system(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut app_exit_events: EventWriter<AppExit>,
+) {
+    // check for input
+    let quit_input = keyboard_input.just_released(KeyCode::Escape);
+
+    // quit app if input read
+    if quit_input {
+        app_exit_events.send(AppExit);
+    }
+}
+
+// setup level of the game
+pub fn setup_game_system(
+    mut commands: Commands,
+    sprite_assets: Res<assets::GameAssets>,
+    audio_channel: Res<AudioChannel<crate::GameMusicAudioChannel>>,
+    game_assets: Res<assets::GameAssets>,
+) {
+    audio_channel
+        .play(game_assets.game_music.clone())
+        .fade_in(AudioTween::new(
+            Duration::from_secs_f32(2.0),
+            AudioEasing::Linear,
+        ))
+        .looped();
+
+    // spawn fire line indicator
+    commands
+        .spawn(SpriteBundle {
+            texture: sprite_assets.fire_line_image.clone(),
+            transform: Transform::from_translation(Vec3::new(crate::FIRE_LINE, 0.0, -1.0)),
+            ..Default::default()
+        })
+        .insert(AppStateComponent(AppStates::Game));
+
+    // spawn the ground image
+    commands
+        .spawn(SpriteBundle {
+            texture: sprite_assets.ground_image.clone(),
+            transform: Transform {
+                translation: Vec3::new(0.0, -300.0, -2.0),
+                scale: Vec3::new(1.5, 1.5, 1.0),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(AppStateComponent(AppStates::Game));
+
+    // spawn ground hitbox
+    commands
+        .spawn(RigidBody::Fixed)
+        .insert(Collider::cuboid(10000.0, 50.0))
+        .insert(TransformBundle::from_transform(
+            Transform::from_translation(Vec3::new(0.0, -300.0, 0.0)),
+        ))
+        .insert(Restitution::new(0.35))
+        .insert(Friction::new(0.9))
+        .insert(AppStateComponent(AppStates::Game));
+}
+
+// setup level of the game
+pub fn setup_main_menu_system(
+    menu_music_audio_channel: Res<AudioChannel<crate::MenuMusicAudioChannel>>,
+    menu_assets: Res<assets::MenuAssets>,
+) {
+    menu_music_audio_channel
+        .play(menu_assets.menu_music.clone())
+        .fade_in(AudioTween::new(
+            Duration::from_secs_f32(2.0),
+            AudioEasing::Linear,
+        ))
+        .looped();
+}
+
+pub fn clean_up_main_menu_system(audio_channel: Res<AudioChannel<crate::MenuMusicAudioChannel>>) {
+    audio_channel.stop();
+}
+
+pub fn clean_up_game_system(audio_channel: Res<AudioChannel<crate::GameMusicAudioChannel>>) {
+    audio_channel.stop();
+}
+
+// setup level of the game
+pub fn setup_victory_menu_system(
+    sound_effects_audio_channel: Res<AudioChannel<crate::SoundEffectsAudioChannel>>,
+
+    menu_music_audio_channel: Res<AudioChannel<crate::MenuMusicAudioChannel>>,
+    menu_assets: Res<assets::MenuAssets>,
+) {
+    sound_effects_audio_channel.play(menu_assets.celebration.clone());
+
+    menu_music_audio_channel
+        .play(menu_assets.menu_music.clone())
+        .fade_in(AudioTween::new(
+            Duration::from_secs_f32(2.0),
+            AudioEasing::Linear,
+        ))
+        .looped();
+}
+
+pub fn clean_up_victory_menu_system(
+    audio_channel: Res<AudioChannel<crate::MenuMusicAudioChannel>>,
+) {
+    audio_channel.stop();
+}
+
+// setup level of the game
+pub fn setup_gameover_menu_system(
+    audio_channel: Res<AudioChannel<crate::MenuMusicAudioChannel>>,
+    menu_assets: Res<assets::MenuAssets>,
+) {
+    audio_channel
+        .play(menu_assets.menu_music.clone())
+        .fade_in(AudioTween::new(
+            Duration::from_secs_f32(2.0),
+            AudioEasing::Linear,
+        ))
+        .looped();
+}
+
+pub fn clean_up_gameover_menu_system(
+    audio_channel: Res<AudioChannel<crate::MenuMusicAudioChannel>>,
+) {
+    audio_channel.stop();
+}
