91a21cde86470319754edc7924ce28f7b88f9a14
Joel Schuman <foibles@gmail.com>
2023-02-05 12:05:41 -0600

    dynamic hair heights, change to capsule colliders

--- src/hair/mod.rs
+++ src/hair/mod.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 use bevy_kira_audio::{AudioChannel, AudioControl};
 use bevy_rapier2d::prelude::*;
-use rand::seq::SliceRandom;
+use rand::{seq::SliceRandom, thread_rng, Rng};
 
 use crate::{
     assets::{self, GameAssets},
@@ -9,7 +9,21 @@ use crate::{
     states::{self, AppStates},
 };
 
+const MIN_MIDDLE_SEGMENTS: i32 = 0;
+const MAX_MIDDLE_SEGMENTS: i32 = 3;
+
 pub fn spawn_hair(commands: &mut Commands, game_assets: &assets::GameAssets, position: Vec2) {
+    let hair_width = 12.0;
+    let root_height = 23.0;
+    let seg_collider_height = 20.0;
+    let seg_position_multiplier = 60.0;
+    let joint_top = 30.0;
+    let joint_bottom = -30.0;
+    let top_extra_pos = 82.0;
+
+    let num_mid_segments = thread_rng().gen_range(MIN_MIDDLE_SEGMENTS..=MAX_MIDDLE_SEGMENTS);
+
+    // create root segment
     let root_entity = commands
         .spawn(SpriteBundle {
             texture: game_assets.hair_root_image.clone(),
@@ -17,7 +31,7 @@ pub fn spawn_hair(commands: &mut Commands, game_assets: &assets::GameAssets, pos
             ..Default::default()
         })
         .insert(RigidBody::Fixed)
-        .insert(Collider::cuboid(12.0, 25.0))
+        .insert(Collider::capsule_y(root_height, hair_width))
         .insert(Restitution::new(0.3))
         .insert(states::AppStateComponent(states::AppStates::Game))
         .insert(ActiveEvents::COLLISION_EVENTS)
@@ -29,46 +43,70 @@ pub fn spawn_hair(commands: &mut Commands, game_assets: &assets::GameAssets, pos
         .insert(RootComponent)
         .id();
 
+    let bottom_joint_bottom = if num_mid_segments == 0 { -55.0 } else { -32.0 };
     let bottom_joint = RevoluteJointBuilder::new()
         .local_anchor1(Vec2::new(0.0, 32.0))
-        .local_anchor2(Vec2::new(0.0, -32.0))
+        .local_anchor2(Vec2::new(0.0, bottom_joint_bottom))
         .motor_position(0.0, 800.0, 10.0);
 
-    let bottom_entity = commands
-        .spawn(SpriteBundle {
-            texture: game_assets.hair_bottom_image.clone(),
-            transform: Transform::from_translation(Vec3::new(position.x, position.y + 64.0, -1.0)),
-            ..Default::default()
-        })
-        .insert(RigidBody::Dynamic)
-        .insert(Collider::cuboid(12.0, 25.0))
-        .insert(Restitution::new(0.3))
-        .insert(states::AppStateComponent(states::AppStates::Game))
-        .insert(ImpulseJoint::new(root_entity, bottom_joint))
-        .insert(ActiveEvents::COLLISION_EVENTS)
-        .insert(HairComponent {
-            max_health: 450.0,
-            health: 450.0,
-            broken_image: game_assets.hair_bottom_broken_image.clone(),
-        })
-        .id();
+    // use these to chain to the next midjoint
+    let mut prev_entity = root_entity;
+    let mut prev_joint = bottom_joint;
+    let mut i = 0;
+    while i < num_mid_segments {
+        let mid_entity = commands
+            .spawn(SpriteBundle {
+                texture: game_assets.hair_bottom_image.clone(),
+                transform: Transform::from_translation(Vec3::new(
+                    position.x,
+                    position.y + 64.0 + (i as f32) * seg_position_multiplier,
+                    -1.0,
+                )),
+                ..Default::default()
+            })
+            .insert(RigidBody::Dynamic)
+            .insert(Collider::capsule_y(seg_collider_height, hair_width))
+            .insert(Restitution::new(0.3))
+            .insert(states::AppStateComponent(states::AppStates::Game))
+            .insert(ImpulseJoint::new(prev_entity, prev_joint))
+            .insert(ActiveEvents::COLLISION_EVENTS)
+            .insert(HairComponent {
+                max_health: 450.0,
+                health: 450.0,
+                broken_image: game_assets.hair_bottom_broken_image.clone(),
+            })
+            .id();
 
-    let top_joint = RevoluteJointBuilder::new()
-        .local_anchor1(Vec2::new(0.0, 32.0))
-        .local_anchor2(Vec2::new(0.0, -50.0))
-        .motor_position(0.0, 130.0, 10.0);
+        let mid_joint_bottom = if i == num_mid_segments - 1 {
+            joint_bottom -20.0
+        } else {
+            joint_bottom
+        };
+        let mid_joint = RevoluteJointBuilder::new()
+            .local_anchor1(Vec2::new(0.0, joint_top))
+            .local_anchor2(Vec2::new(0.0, mid_joint_bottom))
+            .motor_position(0.0, 130.0, 10.0);
+
+        i = i + 1;
+        prev_entity = mid_entity;
+        prev_joint = mid_joint;
+    }
 
     let top_entity = commands
         .spawn(SpriteBundle {
             texture: game_assets.hair_top_image.clone(),
-            transform: Transform::from_translation(Vec3::new(position.x, position.y + 82.0, -1.0)),
+            transform: Transform::from_translation(Vec3::new(
+                position.x,
+                position.y + top_extra_pos + (i as f32) * seg_position_multiplier,
+                -1.0,
+            )),
             ..Default::default()
         })
         .insert(RigidBody::Dynamic)
-        .insert(Collider::cuboid(5.0, 40.0))
+        .insert(Collider::capsule_y(43.0, 6.0))
         .insert(Restitution::new(0.3))
         .insert(states::AppStateComponent(states::AppStates::Game))
-        .insert(ImpulseJoint::new(bottom_entity, top_joint))
+        .insert(ImpulseJoint::new(prev_entity, prev_joint))
         .insert(ActiveEvents::COLLISION_EVENTS)
         .insert(HairComponent {
             max_health: 400.0,
